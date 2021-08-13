4f61da388ef7b1a276e5801c299f4b562cec9eb4
petekaroon <pete_k92@hotmail.com>
2021-08-13 16:01:18 +0700

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.7",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    // ropsten: {
+    //   url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+    //   accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+    // },
+  },
+};
